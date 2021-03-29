0d2b9c918abc1ca25652cef0f450eb48833efef5
guicalmeida <guica.almeida@gmail.com>
2021-03-29 08:45:17 -0300

    Weather API added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import {renderElement, generateMenu, generate, changeTab} from "./shortcutFunctions";
-import {loadHome} from "./home";
-import {resumo} from "./espaco";
+import {
+  renderElement,
+  generateMenu,
+  generate,
+  changeTab,
+} from "./shortcutFunctions";
+import { loadHome } from "./home";
+import { resumo } from "./espaco";
 import { calendar, lodgingValue, priceH1 } from "./calendario";
-import {map} from "./localizacao"
+import { map, weatherDiv } from "./localizacao";
 import { contatoDiv } from "./contato";
-
 
 let body = document.querySelector("body");
 let contentDiv = document.querySelector("#content");
@@ -19,64 +23,49 @@ active.setAttribute("style", "margin: 0 auto 0 8%");
 
 //function to add motion do the active Div when areas are accessed via home buttons
 function homeBtnsListener() {
-    let leftBtn = document.querySelector(".button1");
-    let rightBtn = document.querySelector(".button2");
-    leftBtn.addEventListener("click", e => {
-        changeTab(resumo());
-        active.setAttribute("style", "margin: 0 auto 0 26%");  
-    });
-    rightBtn.addEventListener("click", e => {
-        changeTab(
-            priceH1,
-            calendar
-        );
-        active.setAttribute("style", "margin: 0 auto 0 44%");
-        lodgingValue();
-    });
-};
+  let leftBtn = document.querySelector(".button1");
+  let rightBtn = document.querySelector(".button2");
+  leftBtn.addEventListener("click", (e) => {
+    changeTab(resumo());
+    active.setAttribute("style", "margin: 0 auto 0 26%");
+  });
+  rightBtn.addEventListener("click", (e) => {
+    changeTab(priceH1, calendar);
+    active.setAttribute("style", "margin: 0 auto 0 44%");
+    lodgingValue();
+  });
+}
 homeBtnsListener();
-
 
 //Buttons
 let homeBtn = document.querySelector("#item1");
-homeBtn.addEventListener("click", e => {
-    changeTab(
-        loadHome()
-    );
-    active.setAttribute("style", "margin: 0 auto 0 8%");
-    homeBtnsListener();
-})
+homeBtn.addEventListener("click", (e) => {
+  changeTab(loadHome());
+  active.setAttribute("style", "margin: 0 auto 0 8%");
+  homeBtnsListener();
+});
 
 let espacoBtn = document.querySelector("#item2");
-espacoBtn.addEventListener("click", function(){
-    changeTab(
-        resumo()
-    );
-    active.setAttribute("style", "margin: 0 auto 0 26%");
-})
+espacoBtn.addEventListener("click", function () {
+  changeTab(resumo());
+  active.setAttribute("style", "margin: 0 auto 0 26%");
+});
 
 let datasBtn = document.querySelector("#item3");
-datasBtn.addEventListener("click", function(){   
-    changeTab(
-        priceH1,
-        calendar
-    );
-    active.setAttribute("style", "margin: 0 auto 0 44%");
-    lodgingValue();
-})
+datasBtn.addEventListener("click", function () {
+  changeTab(priceH1, calendar);
+  active.setAttribute("style", "margin: 0 auto 0 44%");
+  lodgingValue();
+});
 
 let localBtn = document.querySelector("#item4");
-localBtn.addEventListener("click", function(){   
-    changeTab(
-        map
-    );
-    active.setAttribute("style", "margin: 0 auto 0 62%");
-})
+localBtn.addEventListener("click", function () {
+  changeTab(weatherDiv, map);
+  active.setAttribute("style", "margin: 0 auto 0 62%");
+});
 
 let contatoBtn = document.querySelector("#item5");
-contatoBtn.addEventListener("click", function(){   
-    changeTab(
-        contatoDiv
-    );
-    active.setAttribute("style", "margin: 0 auto 0 80%");
-})
+contatoBtn.addEventListener("click", function () {
+  changeTab(contatoDiv);
+  active.setAttribute("style", "margin: 0 auto 0 80%");
+});
--- src/localizacao.js
+++ src/localizacao.js
@@ -1,9 +1,41 @@
-import { generate } from "./shortcutFunctions";
+import { generate, multiAppend } from "./shortcutFunctions";
 
-
-let map = generate("iframe", "map");
-map.setAttribute("src", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14631.53322444712!2d-47.72204890137226!3d-23.536699182187807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc9ce0441e260b99!2zU8OtdGlvIFPDo28gTHVpeg!5e0!3m2!1spt-BR!2sbr!4v1616552850573!5m2!1spt-BR!2sbr");
+const map = generate("iframe", "map");
+map.setAttribute(
+  "src",
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14631.53322444712!2d-47.72204890137226!3d-23.536699182187807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc9ce0441e260b99!2zU8OtdGlvIFPDo28gTHVpeg!5e0!3m2!1spt-BR!2sbr!4v1616552850573!5m2!1spt-BR!2sbr"
+);
 map.setAttribute("allowfullscreen", true);
 map.setAttribute("loading", "lazy");
 
-export{map}
+const weatherDiv = generate("div", "weatherDiv");
+let weatherTitle = generate("h1", "weatherTitle");
+let tempDiv = generate("div", "allTempDiv");
+
+let tempNowDiv = generate("div", "tempDiv");
+let tempNow = generate("p", "temp");
+let tempNowTxt = generate("p", "tempTxt", "Temperatura");
+multiAppend(tempNowDiv, tempNowTxt, tempNow);
+
+let tempFeelDiv = generate("div", "tempDiv");
+let tempFeel = generate("p", "temp");
+let tempFeelTxt = generate("p", "tempTxt", "Sensação térmica");
+multiAppend(tempFeelDiv, tempFeelTxt, tempFeel);
+
+let moreInfo = generate("a", "moreInfo", "Open Weather Map >");
+moreInfo.href = "https://openweathermap.org/city/3471849";
+moreInfo.target = "_blank";
+multiAppend(tempDiv, tempNowDiv, tempFeelDiv);
+multiAppend(weatherDiv, weatherTitle, tempDiv, moreInfo);
+(async function getWeather() {
+  const response = await fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=Ara%C3%A7oiaba%20da%20serra&lang=pt_br&appid=ee5f5677a63d2406b5a967e5d2beee70&units=metric",
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  tempNow.innerText = Math.round(weatherData.main.temp) + "ºC";
+  tempFeel.innerText = Math.round(weatherData.main.feels_like) + "ºC";
+  weatherTitle.innerText =
+    weatherData.weather[0].description + " em Araçoiaba da Serra - SP";
+})();
+export { map, weatherDiv };
